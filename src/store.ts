f7d440aa525ba9acb84a0de7bbde0860d56f1e6a
Jan <janhendrikstruve@gmail.com>
2022-03-09 17:16:04 +0100

    ✨ Modify store.ts to include api slice

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,16 @@
 // wie createStore (nimmt reducer und erstellt store), uebernimmt aber auch 
 // die implementation von Redux Dev-Tools, Thunk und mehr
 import { configureStore } from '@reduxjs/toolkit'
+import { apiSlice } from './features/apiSlice/apiSlice'
 import counterReducer from './features/counterSlice/counterSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer
-  }
+    [apiSlice.reducerPath]: apiSlice.reducer
+  },
+  middleware: getDefaultMiddleware => (
+    getDefaultMiddleware().concat(apiSlice.middleware)
+  )
 })
 
 // export wichtiger types vom dispatch und vom State
