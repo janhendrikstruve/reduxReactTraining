6f2f51d2a1710ef9c0999b74883f3ded2636040e
Jan <janhendrikstruve@gmail.com>
2022-03-09 17:10:31 +0100

    🏷️ Add types from fetched data and from fetching query

--- src/features/apiSlice/apiSlice.ts
+++ src/features/apiSlice/apiSlice.ts
@@ -1,12 +1,43 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+interface Character {
+    "info": {
+      "count": number,
+      "pages": number,
+      "next": null | string,
+      "prev": null | string
+    },
+    "results":
+      {
+        "id": number,
+        "name": string,
+        "status": string,
+        "species": string,
+        "type": string,
+        "gender": string,
+        "origin": {
+          "name": string,
+          "url": string
+        },
+        "location": {
+          "name": string,
+          "url": string
+        },
+        "image": string,
+        "episode": string[],
+        "url": string,
+        "created": string
+      }[]
+}
+
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://rickandmortyapi.com/api' }),
   endpoints: builder => ({
-    getPosts: builder.query({
-      query: () => '/character'
+      getCharacters: builder.query<Character, void>({
+      query: () => '/character' 
     })
   })
 })
 
+export const { useGetCharactersQuery } = apiSlice
