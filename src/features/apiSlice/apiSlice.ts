011467efa111f14ca221f044310d01d4ea0eecc5
Jan <janhendrikstruve@gmail.com>
2022-03-09 16:00:52 +0100

    ✨ Add api fetching slice

---
+++ src/features/apiSlice/apiSlice.ts
@@ -0,0 +1,12 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://rickandmortyapi.com/api' }),
+  endpoints: builder => ({
+    getPosts: builder.query({
+      query: () => '/character'
+    })
+  })
+})
+
