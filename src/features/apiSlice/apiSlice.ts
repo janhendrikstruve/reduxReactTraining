92cc7924506f46b2ac69d8c2c540fc577af0587d
Jan <janhendrikstruve@gmail.com>
2022-03-11 10:51:47 +0100

    🐛 "Fix" typing bug by removing type declaration of fetched data

--- src/features/apiSlice/apiSlice.ts
+++ src/features/apiSlice/apiSlice.ts
@@ -1,11 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { fetchedDataType } from '../../types'
 
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://rickandmortyapi.com/api' }),
   endpoints: builder => ({
-      getCharacters: builder.query<fetchedDataType, number>({
+      getCharacters: builder.query({
       query: (page: number) => `/character/?page=${page}` 
     }),
   })
