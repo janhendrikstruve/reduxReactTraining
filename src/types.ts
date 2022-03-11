5c7b4c2857c58bd72507864644b94966d8dc5ba1
Jan <janhendrikstruve@gmail.com>
2022-03-11 20:36:50 +0100

    🔥 Remove useless types

--- src/types.ts
+++ src/types.ts
@@ -6,53 +6,53 @@ export interface CharacterType {
   image: string
 }
 
-export interface fetchedDataType {
-  "info": {
-    "count": number,
-    "pages": number,
-    "next": null | string,
-    "prev": null | string
-  },
-  "results":
-    {
-      "id": number,
-      "name": string,
-      "status": string,
-      "species": string,
-      "type": string,
-      "gender": string,
-      "origin": {
-        "name": string,
-        "url": string
-      },
-      "location": {
-        "name": string,
-        "url": string
-      },
-      "image": string,
-      "episode": string[],
-      "url": string,
-      "created": string
-    }[]
-}
+// export interface fetchedDataType {
+//   "info": {
+//     "count": number,
+//     "pages": number,
+//     "next": null | string,
+//     "prev": null | string
+//   },
+//   "results":
+//     {
+//       "id": number,
+//       "name": string,
+//       "status": string,
+//       "species": string,
+//       "type": string,
+//       "gender": string,
+//       "origin": {
+//         "name": string,
+//         "url": string
+//       },
+//       "location": {
+//         "name": string,
+//         "url": string
+//       },
+//       "image": string,
+//       "episode": string[],
+//       "url": string,
+//       "created": string
+//     }[]
+// }
 
-export interface fetchedCharacterType {
-      id: number,
-      "name": string,
-      "status": string,
-      "species": string,
-      "type": string,
-      "gender": string,
-      "origin": {
-        "name": string,
-        "url": string
-      },
-      "location": {
-        "name": string,
-        "url": string
-      },
-      "image": string,
-      "episode": string[],
-      "url": string,
-      "created": string
-}
+// export interface fetchedCharacterType {
+//       id: number,
+//       "name": string,
+//       "status": string,
+//       "species": string,
+//       "type": string,
+//       "gender": string,
+//       "origin": {
+//         "name": string,
+//         "url": string
+//       },
+//       "location": {
+//         "name": string,
+//         "url": string
+//       },
+//       "image": string,
+//       "episode": string[],
+//       "url": string,
+//       "created": string
+// }
